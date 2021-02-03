5ff4a7f04f89391d054ad44c93a05206aed4c318
Haley Warson <haleywarson@gmail.com>
2021-02-03 02:19:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,8 +4,7 @@ def square_array(array)
   new_array = []
   
   while array.length < i
-    new_nums = array[i] * array[i]
-    new_array << new_nums
+    new_array << array[i] * array[i]
     i += 1
   end
   
